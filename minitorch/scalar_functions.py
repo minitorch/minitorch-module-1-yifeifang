40b8b0231a002c9deb17edc7c29c9c16935aa05b
Dewey <deweyfang@gmail.com>
2024-09-25 00:53:13 -0700

    so close

--- minitorch/scalar_functions.py
+++ minitorch/scalar_functions.py
@@ -188,8 +188,7 @@ class LT(ScalarFunction):
 
     @staticmethod
     def backward(ctx: Context, d_output: float) -> Tuple[float, float]:
-        # TODO: Implement for Task 1.4.
-        raise NotImplementedError("Need to implement for Task 1.4")
+        return 0.0, 0.0
 
 
 class EQ(ScalarFunction):
@@ -201,5 +200,4 @@ class EQ(ScalarFunction):
 
     @staticmethod
     def backward(ctx: Context, d_output: float) -> Tuple[float, float]:
-        # TODO: Implement for Task 1.4.
-        raise NotImplementedError("Need to implement for Task 1.4")
+        return 0.0, 0.0
